2a7c20b5ab5c4f932920889d33427e1d41e1e48f
bujyu <bujyu.uo@gmail.com>
2024-03-20 00:41:16 +0800

    fix model rotation issue imported by GBFRBlenderTools

--- mot/importer/rotationWrapperObj.py
+++ mot/importer/rotationWrapperObj.py
@@ -6,7 +6,13 @@ def objRotationWrapper(obj: bpy.types.Object):
 		and abs(obj.parent.rotation_euler[0] - radians(90)) < 0.001 \
 		and obj.rotation_euler[0] == 0:
 		return
-	
+
+	# Revert rotation if Z-up (rotation is made by GBFRBlenderTool)
+	rootBone = obj.bones["_900"]
+	if abs(rootBone.tail[2] - 0.05) < 0.001:
+		obj.rotation_euler[0] = radians(-90)
+		bpy.ops.object.transform_apply(location=False, rotation=True, scale=False, properties=False, isolate_users=False)
+
 	# make invisible parent with 90° rotation on x axis
 	parentObj = bpy.data.objects.new("RotationWrapper", None)
 	parentObj.hide_viewport = True
